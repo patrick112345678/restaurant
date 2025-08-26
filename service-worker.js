68c4c5fe0a74146d0f0a9487393101ed00c374a8
patrick112345678 <131328482+patrick112345678@users.noreply.github.com>
2025-08-26 10:57:17 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE = "slot-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "manifest.webmanifest",
+  // 如果你有 icons 檔，記得加進來；沒放也沒關係，iOS 會用畫面快照當圖示
+  // "icons/icon-192.png",
+  // "icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
